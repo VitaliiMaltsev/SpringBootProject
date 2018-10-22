61fce6b1638a026a052d70fff374028ee552136f
Vitaliy <ihbectop@gmail.com>
2018-10-22 20:19:41 +0300

    error type

--- src/main/java/appPackage/config/CustomErrorController.java
+++ src/main/java/appPackage/config/CustomErrorController.java
@@ -19,16 +19,16 @@ public class CustomErrorController implements ErrorController {
             Integer statusCode = Integer.valueOf(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "/errors/404";
+                return "errors/404";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "/errors/500";
+                return "errors/500";
             }
             else if(statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "/errors/403";
+                return "errors/403";
             }
         }
-        return "/errors/404";
+        return "errors/404";
     }
 
     @Override
